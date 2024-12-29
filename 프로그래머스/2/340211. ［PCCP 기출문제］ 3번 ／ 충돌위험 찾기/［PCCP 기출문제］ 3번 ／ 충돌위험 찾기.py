6875e86f25fd7da783a18198ad4f405167fc5194
LCW <101385274+Cha-nu@users.noreply.github.com>
2024-12-29 23:40:01 +0900

    [level 2] Title: [PCCP 기출문제] 3번 / 충돌위험 찾기, Time: 514.34 ms, Memory: 86.3 MB -BaekjoonHub

--- 프로그래머스/2/340211. ［PCCP 기출문제］ 3번 ／ 충돌위험 찾기/［PCCP 기출문제］ 3번 ／ 충돌위험 찾기.py
+++ 프로그래머스/2/340211. ［PCCP 기출문제］ 3번 ／ 충돌위험 찾기/［PCCP 기출문제］ 3번 ／ 충돌위험 찾기.py
@@ -1,48 +1,52 @@
-from collections import Counter
-
 def solution(points, routes):
     answer = 0
     n = len(routes) # 로봇의 개수
     m = len(routes[0]) # 이동경로 개수
-    robot = {}
+    moves = {} # key: 시간, value: 시간별 로봇의 위치
     
-    for r in range(n):
+    for i in range(n):
         time = 0
-        x, y = points[routes[r][0] - 1][0], points[routes[r][0] - 1][1]
+        r, c = points[routes[i][0] - 1][0], points[routes[i][0] - 1][1]
   
         # 시작 위치 설정
-        if time not in robot:
-            robot[time] = []
-        robot[time].append((x, y))
+        if time not in moves:
+            moves[time] = []
+        moves[time].append((r, c))
 
-        for i in range(1, m):
-            arrive_x, arrive_y = points[routes[r][i] - 1][0], points[routes[r][i] - 1][1]
+        for j in range(1, m):
+            arrive_r, arrive_c = points[routes[i][j] - 1][0], points[routes[i][j] - 1][1]
 
-            while x != arrive_x:
-              if x > arrive_x:
-                x -= 1
-              elif x < arrive_x:
-                x += 1
+            while r != arrive_r:
+              if r > arrive_r:
+                r -= 1
+              elif r < arrive_r:
+                r += 1
               time += 1
-              if time not in robot:
-                robot[time] = []
-              robot[time].append((x, y))
+              if time not in moves: # 예외처리
+                moves[time] = []
+              moves[time].append((r, c))
 
-            while y != arrive_y:
-              if y > arrive_y:
-                y -= 1
-              elif y < arrive_y:
-                y += 1
+            while c != arrive_c:
+              if c > arrive_c:
+                c -= 1
+              elif c < arrive_c:
+                c += 1
               time += 1
-              if time not in robot:
-                robot[time] = []
-              robot[time].append((x, y))
+              if time not in moves:
+                moves[time] = []
+              moves[time].append((r, c))
 
     
-    for key in robot:
-        c = Counter(robot[key])
-        for key in c:
-            if c[key] > 1:
-                answer += 1
+    for key in moves:
+        visited = set()
+        duplicates = set()
+  
+        for coord in moves[key]:
+            if coord in visited:
+                duplicates.add(coord)  # 중복된 좌표를 저장
+            else:
+                visited.add(coord)
+      
+        answer += len(duplicates)  # 중복된 좌표 개수 추가
                 
     return answer
