41e83a829514228c993d3d269f079f84650abb15
LCW <101385274+Cha-nu@users.noreply.github.com>
2025-01-05 00:34:24 +0900

    Update ［PCCE 기출문제］ 10번 ／ 공원.py

--- 프로그래머스/1/340198. ［PCCE 기출문제］ 10번 ／ 공원/［PCCE 기출문제］ 10번 ／ 공원.py
+++ 프로그래머스/1/340198. ［PCCE 기출문제］ 10번 ／ 공원/［PCCE 기출문제］ 10번 ／ 공원.py
@@ -1,27 +1,29 @@
 def solution(mats, park):
-    def spread(m, i, j):
-        for k in range(m):
-            for l in range(m):
-                try:
-                    if park[i+k][j+l] != "-1":
-                        return -1
-                except:
-                    return -1
-        return m
-    
-    answer = -1
-    mats.sort(reverse=True)
-    
-    for m in mats:
-        for i in range(len(park)):
-            for j in range(len(park[i])):
-                if park[i][j] == "-1":
-                    answer = max(answer, spread(m, i, j))
-                else:
-                    continue
-                if answer >= m:
-                    break
-            if answer >= m:
-                break
-    
-    return answer
+  def check(m, i, j):
+    # 매트를 놓을 수 있는지 검사
+    if i + m > len(park) or j + m > len(park[0]):
+      return False  # 범위를 초과하면 실패
+    for k in range(m):
+      for l in range(m):
+        if park[i + k][j + l] != "-1":  # 매트를 놓을 수 없으면 실패
+          return False
+    return True
+
+  answer = -1
+  placed = False  # 매트 배치 여부 플래그
+  mats.sort(reverse=True)  # 큰 매트부터 확인
+
+  for m in mats:
+    for i in range(len(park)):
+      if placed:
+        break
+      for j in range(len(park[i])):
+        if park[i][j] == "-1":  # 빈 공간일 경우 매트 배치 시도
+          if check(m, i, j):
+            answer = max(answer, m)
+            placed = True  # 매트를 배치했으면 중단
+            break
+    if placed:  # 매트를 배치했으면 더 작은 매트를 확인할 필요 없음
+      break
+
+  return answer
