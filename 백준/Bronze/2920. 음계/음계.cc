52dfffe43993e188be1906067a2a62d57a52124f
LCW <101385274+Cha-nu@users.noreply.github.com>
2025-05-25 22:05:14 +0900

    [Bronze II] Title: 음계, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2920. 음계/음계.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[8];
+    for(int i = 0; i < 8; ++i) cin >> arr[i];
+
+    bool asc = true, desc = true;
+    for(int i = 0; i < 7; ++i) {
+        if(arr[i] < arr[i+1]) desc = false;
+        if(arr[i] > arr[i+1]) asc = false;
+    }
+
+    if(asc) cout << "ascending\n";
+    else if(desc) cout << "descending\n";
+    else cout << "mixed\n";
+}
