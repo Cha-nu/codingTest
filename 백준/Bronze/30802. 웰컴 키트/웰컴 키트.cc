4f06870508c612eb0577681f2365a63459d42a94
LCW <101385274+Cha-nu@users.noreply.github.com>
2025-05-26 15:21:18 +0900

    [Bronze III] Title: 웰컴 키트, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/30802. 웰컴 키트/웰컴 키트.cc
+++ 백준/Bronze/30802. 웰컴 키트/웰컴 키트.cc
@@ -1,18 +1,14 @@
 #include <iostream>
 
-int main(){
-    int N, a[6], b[2];
-    int T = 0;
+int main() {
+    int N, a[6], b[2], T = 0;
     std::cin >> N;
-    std::cin >> a[0] >> a[1] >> a[2] >> a[3] >> a[4] >> a[5];
+    for(int &x : a) std::cin >> x;
     std::cin >> b[0] >> b[1];
 
-    for(int i=0; i < 6; i++){
-        T += a[i] / b[0];
-        if (a[i] % b[0] != 0){
-            T += 1;
-        }
-    }
-    std::cout << T << "\n" << N / b[1] << " " << N % b[1];
+    for(int i = 0; i < 6; ++i)
+        T += (a[i] + b[0] - 1) / b[0];
+
+    std::cout << T << '\n' << N / b[1] << ' ' << N % b[1];
     return 0;
 }
