118eede8a4437d5108aa6c353f517bf356b80ec5
LCW <101385274+Cha-nu@users.noreply.github.com>
2025-05-27 16:04:57 +0900

    [Bronze II] Title: Hashing, Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.cc
+++ 백준/Bronze/15829. Hashing/Hashing.cc
@@ -2,17 +2,19 @@
 #include <cmath>
 
 int main(){
-    int r = 31;
-    int M = 1234567891;
+    const int r = 31;
+    const int M = 1234567891;
     int L;
     char s[101];
     long long hash = 0;
+    long long pow_r = 1;
 
     std::cin >> L >> s;
     
     for(int i = 0; i < L; i++){
-        hash += ((s[i] - 'a' + 1) * (long long)pow(r, i));
+        hash = (hash + (s[i] - 'a' + 1) * pow_r) % M;
+        pow_r = (pow_r * r) % M;
     }
 
-    std::cout << hash % M;
+    std::cout << hash;
 }
