8760f75f33cc1867da8cf86137dd9b4dc35b914f
LCW <101385274+Cha-nu@users.noreply.github.com>
2025-06-27 20:57:18 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main() {
+    int T = 0;
+    std::cin >> T;
+    int dp[15][15] = {0};
+    for(int i = 0; i < 15; i++){
+        dp[0][i] = i;
+        dp[i][1] = 1;
+    }
+    for(int i = 0; i < T; i++){
+        int k = 0, n = 0;
+        std::cin >> k >> n;
+        for(int j = 0; j < k; j++){
+            for(int l = 0; l < n; l++){
+                dp[j + 1][l + 1] = dp[j][l + 1] + dp[j + 1][l];
+            }
+        }
+        std::cout << dp[k][n] << "\n"; 
+    }
+}
