9e8b2b7088023b76d8a0cfd195260f0e029b15be
LCW <101385274+Cha-nu@users.noreply.github.com>
2025-06-29 18:55:13 +0900

    [Bronze I] Title: FizzBuzz, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/28702. FizzBuzz/FizzBuzz.cc
+++ 백준/Bronze/28702. FizzBuzz/FizzBuzz.cc
@@ -12,28 +12,13 @@ int main(){
     c2 = atoi(c.c_str());
 
     // result는 이중 0이 아닌 값 찾기 a가 0이 아니면 +3, b가 0이 아니면 +2 c가 0이 아니면 +1
-    if(a2 != 0){
-        result = a2 + 3;
-    }
-    else if(b2 != 0){
-        result = b2 + 2;
-    }
-    else{
-        result = c2 + 1;
-    }
+    if(a2 != 0) result = a2 + 3;
+    else if(b2 != 0) result = b2 + 2;
+    else result = c2 + 1;
 
-    if (result % 3 == 0){
-        if(result % 5 == 0){
-            cout << "FizzBuzz";
-        }
-        else cout <<"Fizz";
-    } 
-    else if (result % 5 == 0)
-    {
-        cout << "Buzz";
-    }
-    else{
-        cout << result;
-    }
+    if (result % 15 == 0) cout << "FizzBuzz";
+    else if (result % 3 == 0) cout << "Fizz";
+    else if (result % 5 == 0) cout << "Buzz";
+    else cout << result;
     return 0;
 }
