61a94334f7f800b4e169593245b3b5b831af9b12
LCW <101385274+Cha-nu@users.noreply.github.com>
2025-07-17 19:51:10 +0900

    [Silver IV] Title: 요세푸스 문제 0, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.cc
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.cc
@@ -1,38 +1,31 @@
 #include <iostream>
-#include <vector>
+#include <list>
 
 using namespace std;
 
-int check(int size, int i){
-    if(i > size) return i % size;
-    else return i;
-}
-
-int main(){
+int main() {
     int N, K;
     cin >> N >> K;
 
-    int idx = 0;
-    vector<int> arr(N+1);
-    for (int i = 0; i < N+1; i++) arr[i] = i;
+    list<int> people;
+    for (int i = 1; i <= N; i++) people.push_back(i);
 
+    auto it = people.begin();
     cout << "<";
-    for(int i = 0; i < N-1; i++){
-        for(int j = 0; j < K; j++){
-            idx+=1;
-            idx = check(N, idx);
-            if(arr[idx] == 0){
-                while(arr[idx] == 0){
-                    idx+=1;
-                    idx = check(N, idx);
-                }
-            }
+
+    while (!people.empty()) {
+        // K-1칸 이동 (K번째를 제거할거니까)
+        for (int i = 0; i < K - 1; i++) {
+            it++;
+            if (it == people.end()) it = people.begin();
         }
-        cout << arr[idx] << ", ";
-        arr[idx] = 0;
+
+        cout << *it;
+        it = people.erase(it);  // 현재 위치 삭제하고 다음 원소 가리킴
+        if (!people.empty()) cout << ", ";
+        if (it == people.end()) it = people.begin();
     }
-    for (int i = 0; i < N+1; i++){
-        if(arr[i] != 0) cout << arr[i] << ">";
-    }
+
+    cout << ">";
     return 0;
 }
