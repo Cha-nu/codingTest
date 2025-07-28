e4edbf05f30ae5b04c5002daf96a7182c833ffad
LCW <101385274+Cha-nu@users.noreply.github.com>
2025-07-28 20:23:54 +0900

    [Silver III] Title: 패션왕 신해빈, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.cc
+++ 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.cc
@@ -5,27 +5,24 @@
 using namespace std;
 
 int main(){
-    int T;
+    int T, n, result, cat, key;
+    string name, cate;
     cin >> T;
 
     while(T--){
-        int n;
         cin >> n;
         map<string, int> cloth;
-        string name, cate;
-
+        result = 1;
         while(n--){
             cin >> name >> cate;
             cloth[cate]++;
         }
-
-        int result = 1;
-        for(auto& c : cloth){
-            result *= (c.second + 1);  // (입는 경우 + 안 입는 경우)
+        if(key >= 2) cat = 1;
+        // 전체 합, 곱 계산
+        for(auto a = cloth.begin(); a != cloth.end(); a++){
+            result *= (a->second+1);
         }
-
-        result -= 1;  // 아무것도 안 입는 경우 제외
-        cout << result << '\n';
+        cout << --result << '\n';
     }
     return 0;
 }
