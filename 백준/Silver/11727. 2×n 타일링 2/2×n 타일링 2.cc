93ba123e13e82a6fbcceb7a79c7fc7f0c019411d
LCW <101385274+Cha-nu@users.noreply.github.com>
2025-07-29 19:36:50 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    vector<unsigned long long> memo(n+1);
+    memo[0] = 0;
+    memo[1] = 1;
+    memo[2] = 3;
+
+    for(int i = 3; i <= n; i++){
+        memo[i] = (2*memo[i-2] + memo[i-1]) % 10007 ;
+    }
+
+    cout << memo[n] << '\n';
+    return 0;
+}
