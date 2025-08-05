9e6db6921af490b28ca2d9efeb3c1c8841278b47
LCW <101385274+Cha-nu@users.noreply.github.com>
2025-08-05 16:50:35 +0900

    [Silver I] Title: IOIOI, Time: 44 ms, Memory: 4916 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.cc
+++ 백준/Silver/5525. IOIOI/IOIOI.cc
@@ -1,27 +1,27 @@
 #include <iostream>
 #include <string>
-
 using namespace std;
 
-int main(){
+int main() {
     int n, m;
     string s;
     cin >> n >> m >> s;
 
-    int len = 2 * n + 1;
-    int i = 0, result = 0;
-    bool match = true;
+    int result = 0;
+    int count = 0;
 
-    while(i <= m - len){
-        if(s[i] == 'I'){
-            match = true;
-            for(int j = 0; j < len; j++){
-                if((j % 2 ==0 && s[i+j] != 'I') || (j % 2 ==1 && s[i+j] != 'O')){match = false; break;}
+    for (int i = 1; i < m - 1;) {
+        if (s[i - 1] == 'I' && s[i] == 'O' && s[i + 1] == 'I') {
+            count++;
+            if (count == n) {
+                result++;
+                count--; // 겹치는 IOI 고려
             }
-            if(match){ result++; i+=2;}
-            else i++;
+            i += 2; // IOI니까 O 다음 I로 점프
+        } else {
+            count = 0;
+            i++; // 불일치니까 한 칸만 이동
         }
-        else i++;
     }
 
     cout << result;
