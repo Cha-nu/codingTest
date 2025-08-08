8975de7a0c8287a423fb134223a5ef5e78b43a1b
LCW <101385274+Cha-nu@users.noreply.github.com>
2025-08-08 16:45:09 +0900

    [Gold V] Title: 토마토, Time: 92 ms, Memory: 5864 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.cc
+++ 백준/Gold/7569. 토마토/토마토.cc
@@ -4,49 +4,41 @@
 #include <tuple>
 using namespace std;
 
-int main() {
+int main(){
     ios::sync_with_stdio(false);
     cin.tie(NULL);
 
     int n, m, h;
     cin >> n >> m >> h;
 
-    // H x M x N 순서로 배열 선언
-    vector<vector<vector<short>>> box(h, vector<vector<short>>(m, vector<short>(n, -1)));
+    vector<vector<vector<short>>> box(h, vector(m, vector<short>(n, -1)));
     queue<tuple<int, int, int>> q;
-    
-    vector<short> dx = {1, -1, 0, 0, 0, 0}; // x축
-    vector<short> dy = {0, 0, 1, -1, 0, 0}; // y축
-    vector<short> dz = {0, 0, 0, 0, 1, -1}; // z축
+    vector<short> dx = {1, -1, 0, 0, 0, 0};
+    vector<short> dy = {0, 0 , 1, -1, 0, 0};
+    vector<short> dz = {0, 0, 0, 0, 1, -1};
     
     int result = -1, count = 0;
     
-    // H > M > N 순서로 입력값 읽기
-    for (short k = 0; k < h; k++) {
-        for (short i = 0; i < m; i++) {
-            for (short j = 0; j < n; j++) {
-                cin >> box[k][i][j];
-                if (box[k][i][j] == 1) {
-                    q.push({k, i, j});
-                }
-                if (box[k][i][j] == 0) {
-                    count++;
-                }
+    for(short i = 0; i < h; i++){ // z 
+        for(short j = 0; j < m; j++){ // y
+            for(short k = 0; k < n; k++){ // x
+                cin >> box[i][j][k];
+                if(box[i][j][k] == 1) q.push({i, j, k});
+                if(box[i][j][k] == 0) count++;
             }
         }
     }
 
-    // BFS 탐색
-    while (!q.empty()) {
+    // bfs
+    while(!q.empty()){
         int sz = q.size();
-        while (sz--) {
+        while(sz--){
             auto [z, y, x] = q.front(); q.pop();
-            for (short i = 0; i < 6; i++) {
-                int nz = z + dz[i];
-                int ny = y + dy[i];
+            for(short i = 0; i < 6; i++){
                 int nx = x + dx[i];
-                
-                if (nz >= 0 && nz < h && ny >= 0 && ny < m && nx >= 0 && nx < n && box[nz][ny][nx] == 0) {
+                int ny = y + dy[i];
+                int nz = z + dz[i];
+                if(nx >= 0 && ny >= 0 && nz >= 0 && nx < n && ny < m && nz < h && box[nz][ny][nx] == 0){
                     q.push({nz, ny, nx});
                     box[nz][ny][nx] = 1;
                     count--;
@@ -56,11 +48,7 @@ int main() {
         result++;
     }
 
-    if (count > 0) {
-        cout << -1;
-    } else {
-        cout << result;
-    }
-    
+    if(count) cout << -1; // 0이 남아있으면
+    else cout << result;
     return 0;
 }
