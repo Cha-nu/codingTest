4fa6dfa4827899eb2c869680ff56a4816aada9ae
LCW <101385274+Cha-nu@users.noreply.github.com>
2025-08-13 16:34:51 +0900

    [Silver III] Title: N과 M (2), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// nCm
+void com(short i, short n, short m, vector<short>& arr){
+    if (m == 0){ // 다 뽑을 시 출력
+        for(int j = 0; j < arr.size(); j++){
+            cout << arr[j] << ' ';
+        }
+        cout << '\n';
+        return;
+    }
+    else if(i > n) return; // i가 n을 넘어가면 끝
+    else
+    {
+        // i를 뽑은 경우
+        arr.push_back(i);
+        com(i+1, n, m-1, arr);
+        arr.pop_back();
+        // i를 뽑지 않은 경우
+        com(i+1, n,  m, arr);
+    }
+}
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    short n, m;
+    cin >> n >> m;
+
+    vector<short> arr;
+
+    com(1, n, m, arr);
+        
+    return 0;
+}
