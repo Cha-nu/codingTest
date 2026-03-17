6a3b9510c41b47f7b9846293bad1395bacba115d
LCW <101385274+Cha-nu@users.noreply.github.com>
2026-03-17 10:28:04 +0900

    [level 2] Title: 괄호 회전하기, Time: 12.54 ms, Memory: 4.17 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.cpp
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.cpp
@@ -1,78 +1,40 @@
-#include <string>
 #include <stack>
-#include <queue>
+#include <string>
+#include <unordered_map>
+#include <vector>
 
 using namespace std;
 
+unordered_map<char, char> bracketPair = {{')', '('},{']', '['}, {'}','{'}};
+
+bool isVaild(string &s, int start)
+{
+    stack<char> stk;
+    unsigned int sz = s.size();
+    
+    for(int i = 0; i < sz; i++)
+    {
+        char ch = s[(start + i) % sz];
+        if(bracketPair.count(ch))
+        {
+            if(stk.empty() || stk.top() != bracketPair[ch]) return false;
+            stk.pop();
+        }
+        else
+        {
+            stk.push(ch);    
+        }
+    }
+    return stk.empty();
+}
+
+
 int solution(string s) {
     int answer = 0;
-    int n = s.size();
     
-    for(int start = 0; start < n; start++)
+    for(int start = 0; start < s.size(); start++)
     {
-        bool key = true;
-        stack<char> st;
-        for(int i = 0; i < n; i++)
-        {
-            char c = s[(start + i) % n];
-            
-            if(c == '(' || c == '{' || c == '[') st.push(c);
-            else if(c == ')')
-            {
-                if(st.empty())
-                {
-                    key = false;
-                    break;
-                }
-                else
-                {
-                    char d = st.top();
-                    st.pop();
-                    if(d != '(')
-                    {
-                        key = false;
-                        break;
-                    }
-                }
-            }
-            else if(c == '}')
-            {  
-                if(st.empty())
-                {
-                    key = false;
-                    break;
-                }
-                else
-                {
-                    char d = st.top();
-                    st.pop();
-                    if(d != '{')
-                    {
-                        key = false;
-                        break;
-                    }
-                }
-            }
-            else if(c == ']')
-            {
-                if(st.empty())
-                {
-                    key = false;
-                    break;
-                }
-                else
-                {
-                    char d = st.top();
-                    st.pop();
-                    if(d != '[')
-                    {
-                        key = false;
-                        break;
-                    }
-                }
-            }
-        }
-        if(key && st.empty()) answer++;     
+        answer += isVaild(s, start);
     }
     return answer;
 }
