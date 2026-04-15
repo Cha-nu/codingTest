f796e03c169e2065d2791cf3eedaffb4e4eb8d61
LCW <101385274+Cha-nu@users.noreply.github.com>
2026-04-15 09:03:41 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.01 ms, Memory: 4.13 MB -BaekjoonHub

---
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string s) {
+    string answer = "";
+    int n = static_cast<int>(s.length());
+    if(n % 2 == 0)
+    {
+        answer = s[(n/2)-1];
+        answer += s[n/2];
+    }
+    else answer = s[n/2];
+    return answer;
+}
