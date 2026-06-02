cc9a777092745b68198654a26eecb8d21644dfb7
LCW <101385274+Cha-nu@users.noreply.github.com>
2026-06-02 15:16:58 +0900

    [level 1] Title: 카드 뭉치, Time: 0.02 ms, Memory: 3.94 MB -BaekjoonHub

---
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+string solution(vector<string> cards1, vector<string> cards2, vector<string> goal) {
+    int idx1 = 0;
+    int idx2 = 0;
+    
+    for(string const& s : goal)
+    {
+        if(idx1 < cards1.size() && s == cards1[idx1]) idx1++;
+        else if (idx2 < cards2.size() && s == cards2[idx2]) idx2++;
+        else return "No";
+    }
+    return "Yes";
+}
