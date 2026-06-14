3b614687e0c81ebd2f7dee0f55473a39bd02baee
LCW <101385274+Cha-nu@users.noreply.github.com>
2026-06-14 12:53:50 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.03 ms, Memory: 4.13 MB -BaekjoonHub

---
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<string> wallpaper) {
+    vector<int> answer = {51, 51, 0, 0}; // lux, luy, rdx, rdy
+    
+    for(int i = 0; i < static_cast<int>(wallpaper.size()); i++)
+    {
+        for(int j = 0; j < static_cast<int>(wallpaper[0].size()); j++)
+        {
+            if(wallpaper[i][j] == '#')
+            {
+                answer[0] = min(answer[0], i);
+                answer[1] = min(answer[1], j);
+                answer[2] = max(answer[2], i+1);
+                answer[3] = max(answer[3], j+1);
+            }
+        }
+    }
+    
+    return answer;
+}
