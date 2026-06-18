9135773b4e5d81e63c80cd183e44b273ba47792e
LCW <101385274+Cha-nu@users.noreply.github.com>
2026-06-18 19:05:22 +0900

    [level 1] Title: 신고 결과 받기, Time: 177.54 ms, Memory: 26.5 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.cpp
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.cpp
@@ -1,43 +1,9 @@
 #include <string>
 #include <vector>
-#include <map>
-#include <sstream>
 
 using namespace std;
 
 vector<int> solution(vector<string> id_list, vector<string> report, int k) {
-    int j = id_list.size();
-    vector<int> answer(j, 0);
-    vector<int> ans(j, 0);
-    vector<vector<bool>> ad (j, vector<bool>(j, true));
-    map<string, int> a;
-    
-    for(size_t i = 0; i < j; i++) a[id_list[i]] = i;
-    
-    for(const string& s : report)
-    {
-        stringstream ss(s);
-        string n, m;
-        ss >> n >> m;
-        
-        if(ad[a[n]][a[m]])
-        {
-            ad[a[n]][a[m]] = false; //false 신고
-            ans[a[m]]++;
-        }  
-    }
-    
-    for(int i = 0; i < j ; i++)
-    {
-        if(ans[i] >= k)
-        {
-            for(int t = 0; t < j; t++)
-            {
-                if(!ad[t][i]) answer[t]++;
-            }
-        }
-    }
-    
-    
+    vector<int> answer;
     return answer;
 }
