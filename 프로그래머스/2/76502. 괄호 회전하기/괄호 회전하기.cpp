9df49398a771ecebbf45b8b6055311e9ea30ad64
LCW <101385274+Cha-nu@users.noreply.github.com>
2026-06-27 10:27:30 +0900

    [level 2] Title: 괄호 회전하기, Time: 8.69 ms, Memory: 4.95 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.cpp
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.cpp
@@ -7,7 +7,6 @@ bool isValid(const string &s, int start, int n) {
     stack<char> stk;
     for(int i = 0; i < n; i++) {
         char ch = s[(start + i) % n];
-        // Map 대신 switch를 사용하면 컴파일러가 Jump Table로 최적화하여 매우 빠름
         switch(ch) {
             case '(': case '{': case '[':
                 stk.push(ch);
