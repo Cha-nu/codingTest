60fdebe05cab13c2545af2fd2a40b4f7b90568b3
LCW <101385274+Cha-nu@users.noreply.github.com>
2026-06-29 17:59:37 +0900

    [level 1] Title: 신고 결과 받기, Time: 111.85 ms, Memory: 26.8 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.cpp
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.cpp
@@ -3,39 +3,45 @@
 #include <unordered_map>
 #include <sstream>
 
+
 using namespace std;
 
 vector<int> solution(vector<string> id_list, vector<string> report, int k) {
+    
+    
     int n = static_cast<int>(id_list.size());
     
-    vector<vector<bool>> g (n, vector<bool> (n, true)); // 신고가능여부 행을 신고 한 사람
-    vector<int> r (n, 0);
-    vector<int> answer (n, 0);
-    unordered_map<string, int> idx;
+    vector<int> answer(n, 0); // 결과
     
-    for(int i = 0; i < n; i++) idx[id_list[i]] = i;
+    unordered_map<string, int> um;
     
-    for(const string& re: report) // 신고처리
-    {
-        stringstream ss(re);
-        string x, y;
-        ss >> x >> y;
-        if(g[idx[y]][idx[x]])
-        {
-            g[idx[y]][idx[x]] = false;
-            r[idx[y]]++; // 신고 당한 횟수
-        }
+    for(int i = 0; i < n; ++i) um[id_list[i]] = i;
+    
+    vector<vector<bool>> m (n, vector<bool>(n, false)); // 신고를 한 사람;
+    vector<int> re (n, 0); // 신고 당한 횟수
         
-        
-    }
-    // 정지 메일 받고 정지 보고 메일
-    for(int i = 0; i < n; i++)
+    for(string r : report)
     {
-        if(r[i] >= k)
+        stringstream ss(r);
+        string a, b;
+        ss >> a >> b;
+        int i = um[b];
+        int j = um[a];
+        if(!m[i][j])
         {
-            for(int j = 0; j < n; j++) if(!g[i][j]) answer[j]++; // 신고 한 사람한테 메일
+            m[i][j] = true;
+            ++re[i];
         }
     }
-
+    
+    for(int i = 0; i < n; ++i)
+    {
+        if(re[i] >= k)
+        {
+            for(int j = 0; j < n; ++j) if(m[i][j]) ++answer[j];
+        }
+    }
+    
+    
     return answer;
 }
