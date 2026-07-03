c613cd354c54b982c90b795b0afc1fbf93cfedf7
LCW <101385274+Cha-nu@users.noreply.github.com>
2026-07-04 01:29:52 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 168.51 ms, Memory: 22.3 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.cpp
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.cpp
@@ -3,24 +3,22 @@
 
 using namespace std;
 
-
 int solution(vector<int> elements) {
-    size_t n = elements.size();
-    unordered_set<int> s;
+    int n = static_cast<int>(elements.size());
     
+    unordered_set<int> s;
     vector<int> e = elements;
     e.insert(e.end(), elements.begin(), elements.end());
     
-    
-    for(size_t i = 0; i < n; ++i)
+    for(int i = 0; i < n; ++i)
     {
         int sum = 0;
-        for(size_t j = 0; j < n; ++j)
+        for(int j = 0; j < n; ++j)
         {
             sum += e[i+j];
             s.insert(sum);
         }
     }
     
-    return s.size();
+    return (static_cast<int>(s.size()));
 }
