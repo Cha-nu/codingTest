dce5d7ad004efbeb8de8820a716ff0fdeb179dc9
LCW <101385274+Cha-nu@users.noreply.github.com>
2026-07-04 01:45:32 +0900

    [level 2] Title: n^2 배열 자르기, Time: 26.52 ms, Memory: 24.5 MB -BaekjoonHub

--- 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.cpp
+++ 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.cpp
@@ -6,12 +6,12 @@ vector<int> solution(int n, long long left, long long right) {
     vector<int> answer;
     answer.reserve(right - left + 1);
     
-    for (long long i = left; i <= right; ++i) {
-        long long row = i / n;
-        long long col = i % n;
-        
-        answer.push_back(max(row, col) + 1);
+    for(unsigned long long i = left; i < right+1;++i)
+    {
+        unsigned long long row = i / n;
+        unsigned long long col = i % n;
+        answer.push_back(max(row, col)+1);
     }
-
+    
     return answer;
 }
