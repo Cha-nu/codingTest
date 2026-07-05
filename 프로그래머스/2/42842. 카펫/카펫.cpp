be89a973844e0623f0db10a0ee6be13fb1a7d5c4
LCW <101385274+Cha-nu@users.noreply.github.com>
2026-07-06 02:01:23 +0900

    [level 2] Title: 카펫, Time: 0.01 ms, Memory: 4.82 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.cpp
+++ 프로그래머스/2/42842. 카펫/카펫.cpp
@@ -1,17 +1,18 @@
-#include <string>
 #include <vector>
 
 using namespace std;
 
 vector<int> solution(int brown, int yellow) {
-    int t = brown + yellow;
+    int total = brown + yellow;
     
-    for(int h = 3; h*h <= t; ++h)
+    for(int h = 3; h*h <= total; ++h)
     {
-        int w = t / h;
+        int w = total/h;
         
         if((w-2)*(h-2) == yellow) return {w, h};
     }
     
+    
     return {-1, -1};
 }
+
