6b747d1ef7d92c44619b28a5b7fcfe6ea8dc3cc8
LCW <101385274+Cha-nu@users.noreply.github.com>
2026-07-16 22:54:39 +0900

    [level 2] Title: 2개 이하로 다른 비트, Time: 24.62 ms, Memory: 28.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.cpp
@@ -0,0 +1,20 @@
+#include <vector>
+
+using namespace std;
+
+vector<long long> solution(vector<long long> numbers) {
+    vector<long long> answer;
+    
+    for (long long x : numbers)
+    {
+        if (x % 2 == 0) answer.push_back(x + 1);
+        else
+        {
+            long long bit = 1;
+            while ((x & bit) != 0) bit <<= 1;
+            answer.push_back(x + bit - (bit >> 1));
+        }
+    }
+    
+    return answer;
+}
