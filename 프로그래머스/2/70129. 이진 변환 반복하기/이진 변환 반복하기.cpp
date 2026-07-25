c4f6758fdf9c3d816fc1c6207246585d61f3977e
LCW <101385274+Cha-nu@users.noreply.github.com>
2026-07-25 16:19:45 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 0.65 ms, Memory: 4.7 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.cpp
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.cpp
@@ -1,29 +1,29 @@
 #include <string>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
 vector<int> solution(string s) {
-    vector<int> answer (2, 0);
+    vector<int> answer = {0, 0}; // {이진 변환 횟수, 제거된 0의 갯수}
     
     while(s != "1")
     {
         int cnt = 0;
-        ++answer[0];
-        for(char c: s)
+        // 문자열 x의 길이 세기
+        for(char c : s)
         {
             if(c == '1') ++cnt;
-            else if(c == '0') ++answer[1];
+            else ++answer[1];
         }
         s = "";
-        
-        while(cnt != 0)
+        while(cnt > 0) // 2진수 변환;
         {
-            s += (cnt % 2) +'0';
+            s += cnt % 2 + '0';
             cnt /= 2;
         }
+        ++answer[0];
     }
-    
     
     return answer;
 }
