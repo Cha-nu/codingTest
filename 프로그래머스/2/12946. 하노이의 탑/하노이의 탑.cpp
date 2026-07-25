8a0c3050b778bf520d1df78847dc5809e6fe11b3
LCW <101385274+Cha-nu@users.noreply.github.com>
2026-07-25 17:48:12 +0900

    [level 2] Title: 하노이의 탑, Time: 16.12 ms, Memory: 28.3 MB -BaekjoonHub

--- 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.cpp
+++ 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.cpp
@@ -1,9 +1,31 @@
 #include <string>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
+
+void h(int n, int start, int to, int aux, vector<vector<int>>& answer)
+{
+    if(n == 1)
+    {
+        answer.push_back({start, to});
+        return;
+    }
+    
+    h(n-1, start, aux, to, answer);
+    answer.push_back({start, to});
+    h(n-1, aux, to, start, answer);
+    
+    
+    return;
+}
+
+
 vector<vector<int>> solution(int n) {
     vector<vector<int>> answer;
+    
+    h(n, 1, 3, 2, answer);
+    
     return answer;
 }
