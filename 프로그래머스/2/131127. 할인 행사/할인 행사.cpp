5f10f6ebc54d60f4399c5d5137be20719c8363a3
LCW <101385274+Cha-nu@users.noreply.github.com>
2026-07-26 23:50:01 +0900

    [level 2] Title: 할인 행사, Time: 5.82 ms, Memory: 12.2 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.cpp
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.cpp
@@ -6,34 +6,31 @@ using namespace std;
 
 bool check(unordered_map<string, int>& target, unordered_map<string, int>& window)
 {
-    for (auto const& [item, count] : target)
-    {
-        if (window[item] != count) return false;
-    }
+    for(auto const&[item, count] : target) if(window[item] != count) return false;
     return true;
 }
 
+
+
 int solution(vector<string> want, vector<int> number, vector<string> discount) {
-    int result = 0;
+    int answer = 0;
     
-    unordered_map<string, int> lst;
-    
-    for(int i = 0; i < static_cast<int>(want.size()); ++i) lst[want[i]] = number[i];
-    
+    unordered_map<string, int> target;
     unordered_map<string, int> window;
     
+    for(int i = 0; i < static_cast<int>(want.size()); ++i) target[want[i]] = number[i];
     for(int i = 0; i < 10; ++i) ++window[discount[i]];
     
-    if (check(lst, window)) result++;
+    if(check(target, window)) ++answer;
     
-    for (int i = 10; i < discount.size(); ++i) {
-        string old_item = discount[i - 10];
-        window[old_item]--;
+    for(int i = 10; i < static_cast<int>(discount.size()); ++i)
+    {
+        string olditem = discount[i - 10];
+        --window[olditem];
+        ++window[discount[i]];
         
-        window[discount[i]]++;
-        
-        if (check(lst, window)) result++;
+        if(check(target, window)) ++answer;
     }
     
-    return result;
+    return answer;
 }
