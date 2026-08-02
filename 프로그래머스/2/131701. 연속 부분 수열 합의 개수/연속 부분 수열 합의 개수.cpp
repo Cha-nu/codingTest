5ec9c16fc38dca312c84aaa7e1d34eb7be4e297a
LCW <101385274+Cha-nu@users.noreply.github.com>
2026-08-02 21:45:55 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 67.66 ms, Memory: 22.4 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.cpp
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.cpp
@@ -1,24 +1,27 @@
-#include <unordered_set>
+#include <string>
 #include <vector>
+#include <unordered_set>
 
 using namespace std;
 
 int solution(vector<int> elements) {
-    int n = static_cast<int>(elements.size());
+    int sz = static_cast<int>(elements.size());
     
-    unordered_set<int> s;
-    vector<int> e = elements;
-    e.insert(e.end(), elements.begin(), elements.end());
+    unordered_set<int> s; // 연속 부분 수열의 합
     
-    for(int i = 0; i < n; ++i)
+    // 원형 수열
+    vector<int> c = elements;
+    c.insert(c.end(), elements.begin(), elements.end());
+    
+    for(int i = 0; i < sz; ++i)
     {
-        int sum = 0;
-        for(int j = 0; j < n; ++j)
+        int sum = 0; // 연속 부분 수열 합 계산
+        for(int j = 0; j < sz; ++j)
         {
-            sum += e[i+j];
-            s.insert(sum);
+            sum += c[i+j]; // {0, 0}, {0, 1}, {0, 2} 점점 더해짐
+            s.insert(sum); // 합 중복 제거
         }
     }
     
-    return (static_cast<int>(s.size()));
+    return static_cast<int>(s.size());
 }
