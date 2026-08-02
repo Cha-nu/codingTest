431e3d4561eadb227a67ca7352ae0c965bb5b7e3
LCW <101385274+Cha-nu@users.noreply.github.com>
2026-08-02 17:43:25 +0900

    [level 2] Title: 의상, Time: 0.01 ms, Memory: 4.88 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.cpp
+++ 프로그래머스/2/42578. 의상/의상.cpp
@@ -1,18 +1,16 @@
 #include <string>
 #include <vector>
 #include <unordered_map>
-#include <iostream>
 
 using namespace std;
 
 int solution(vector<vector<string>> clothes) {
     int answer = 1;
-    
     unordered_map<string, int> equip;
     
-    for(auto& c: clothes) ++equip[c[1]];
+    for(auto& c : clothes) ++equip[c[1]];
     
-    for (auto it = equip.begin(); it != equip.end(); ++it) answer *= (it->second + 1);
-  
-    return answer-1;
+    for(auto it = equip.begin(); it != equip.end(); ++it) answer *= (it->second + 1); // 안입는 경우 + 1
+        
+    return answer - 1; // 아예 안입을 경우 제거 
 }
