7f1dad722b459e3da9f69ce58cf286f945269c7d
LCW <101385274+Cha-nu@users.noreply.github.com>
2026-08-04 22:04:25 +0900

    [level 3] Title: 이중우선순위큐, Time: 15.07 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.cpp
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.cpp
@@ -6,28 +6,27 @@
 using namespace std;
 
 vector<int> solution(vector<string> operations) {
-    multiset<int> dq;
+    multiset<int> answer;
     
-    for(const string& op: operations)
+    for(const string& op :operations)
     {
-        stringstream ss(op);
         char c;
-        int val;
+        int num;
+        stringstream ss(op);
         
-        ss >> c >> val;
+        ss >> c >> num;
         
-        if(c == 'I') dq.insert(val);
-        else if(c == 'D')
+        if(c == 'I') answer.insert(num);
+        else if(c == 'D' && !answer.empty())
         {
-            if(!dq.empty())
-            {
-                if(val == 1) dq.erase(*prev(dq.end()));
-                else if(val == -1) dq.erase(dq.begin());
-            }
+            if(num == 1) answer.erase(*prev(answer.end()));
+            else answer.erase(answer.begin());
         }
-        
     }
-    if(dq.empty()) return {0, 0};
     
-    return {*dq.rbegin(), *dq.begin()};
+    if(answer.empty()) return {0, 0};
+    
+    
+    
+    return {*answer.rbegin(), *answer.begin()};
 }
