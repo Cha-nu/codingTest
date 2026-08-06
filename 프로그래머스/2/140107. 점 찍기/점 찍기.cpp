2a5faed04cabee0145d550e273d7808ff447fe80
LCW <101385274+Cha-nu@users.noreply.github.com>
2026-08-06 19:42:09 +0900

    [level 2] Title: 점 찍기, Time: 2.49 ms, Memory: 4.82 MB -BaekjoonHub

--- 프로그래머스/2/140107. 점 찍기/점 찍기.cpp
+++ 프로그래머스/2/140107. 점 찍기/점 찍기.cpp
@@ -2,26 +2,21 @@
 
 using namespace std;
 
-long long solution(int k, int d)
-{
+long long solution(int k, int d) {
     long long answer = 0;
-    long long distanceSquared = 1LL * d * d;
-
+    long long distanceSquared = 1LL * d * d; // d^2;
+    
     for (long long x = 0; x <= d; x += k)
     {
         long long maxYSquared = distanceSquared - x * x;
         long long maxY = static_cast<long long>(sqrt(maxYSquared));
-
-        // 부동소수점 오차 보정
-        while ((maxY + 1) * (maxY + 1) <= maxYSquared)
-            ++maxY;
-
-        while (maxY * maxY > maxYSquared)
-            --maxY;
-
-        // y = 0, k, 2k, ... maxY
+        
+        // 부동 소수점 오차 보정
+        while((maxY+1) * (maxY + 1) <= maxYSquared) ++maxY;
+        
+        while(maxY * maxY > maxYSquared) --maxY;
+        
         answer += maxY / k + 1;
     }
-
     return answer;
 }
