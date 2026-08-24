1c39b77e1b0cf777b0a6d386828c20fea627aa19
LCW <101385274+Cha-nu@users.noreply.github.com>
2026-08-24 10:59:30 +0900

    [level 3] Title: 입국심사, Time: 15.73 ms, Memory: 7.09 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.cpp
+++ 프로그래머스/3/43238. 입국심사/입국심사.cpp
@@ -1,19 +1,23 @@
-#include <vector>
 #include <algorithm>
+#include <vector>
 
 using namespace std;
 
 long long solution(int n, vector<int> times) {
     long long left = 1;
-    long long right = (long long)*min_element(times.begin(), times.end()) * n;
+    long long right = (long long) * min_element(times.begin(), times.end()) * n;
     
     while(left < right)
     {
-        long long mid = (left + right) / 2; //Time
+        long long mid = (left + (right - left) / 2);
+        
         long long m = 0;
-        for(int time: times) m += mid / time;
-        if(m >= n) right = mid;
-        else left = mid + 1;
+        
+        for(int time : times) m += mid / time;
+        
+        if(n > m) left = mid + 1;
+        else right = mid;
     }
+    
     return left;
 }
