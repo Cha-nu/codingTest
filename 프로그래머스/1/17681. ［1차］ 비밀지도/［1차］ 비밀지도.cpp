f77201f082ec9d9915a2073371a08ddd4b0aeb25
LCW <101385274+Cha-nu@users.noreply.github.com>
2026-09-24 19:26:22 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.01 ms, Memory: 4.76 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.cpp
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.cpp
@@ -1,38 +1,24 @@
 #include <string>
 #include <vector>
-#include <algorithm>
 
 using namespace std;
-
-
-string binary(int n, int a)
-{
-    string b;
-    
-    for(int i = 0; i < n; ++i)
-    {
-        b.push_back(a % 2 + '0');
-        a /= 2;
-    }
-    reverse(b.begin(), b.end());
-    return b;
-}
-
 
 vector<string> solution(int n, vector<int> arr1, vector<int> arr2) {
     vector<string> answer;
     
     for(int i = 0; i < n; ++i)
     {
-        string a = binary(n, arr1[i]);
-        string b = binary(n, arr2[i]);
-        string c;
-        for(int j = 0; j < n; ++j)
+        int map = arr1[i] | arr2[i]; // 비트 연산
+        string row(n, ' ');
+        
+        for(int j = n-1; j >= 0; --j)
         {
-            if(a[j] == '1' || b[j] == '1') c.push_back('#');
-            else c.push_back(' ');
+            if(map & 1) row[j] = '#';
+            
+            map >>= 1;
         }
-        answer.push_back(c);
+        answer.push_back(row);
+        
     }
     
     return answer;
